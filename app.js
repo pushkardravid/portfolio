163ee8a733144e2a4da2c033a0bc3a26eed9cb61
Pushkar <pushkarsdravid@gmail.com>
2020-01-13 02:01:11 -0500

    man portfolio text add

--- app.js
+++ app.js
@@ -10,6 +10,43 @@ function addMessage(message){
   $("#terminal").append(message + "<br>");
 }
 
+function addHelp(){
+  var body = `<div>
+      Commands <br>
+      <table>
+          <tr>
+              <td>education</td>
+              <td>Education bro</td>
+          </tr>
+          <tr>
+              <td>skills</td>
+              <td>Education bro</td>
+          </tr>
+          <tr>
+              <td>experience</td>
+              <td>Education bro</td>
+          </tr>
+          <tr>
+              <td>projects</td>
+              <td>Education bro</td>
+          </tr>
+          <tr>
+              <td>achievements</td>
+              <td>Modest? not</td>
+          </tr>
+          <tr>
+              <td>contact</td>
+              <td>Them contact details</td>
+          </tr>
+          <tr>
+              <td>download</td>
+              <td>Download a copy of my resume</td>
+          </tr>
+      </table>
+  </div>`;
+  $("#terminal").append(body);
+}
+
 $(function() {
     addInput(1);
 });
@@ -20,6 +57,7 @@ $(document).on('keypress',function(e) {
   if(activeElement.tagName !="INPUT"){
     return;
   }
+
   if(e.which == 13) {
       var inputCommand = activeElement.value;
       if(inputCommand == "clear"){
@@ -34,7 +72,7 @@ $(document).on('keypress',function(e) {
         addMessage("education  skills  experience  projects  achievements  contact  download");
         break;
         case "man portfolio":
-          addMessage("portfolio");
+          addHelp();
           break;
         case "education":
           addMessage("education");
